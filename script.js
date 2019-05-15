0269504ade8432504000225f4c514c89888deca4
jtkfrancisco <43385696+jtkfrancisco@users.noreply.github.com>
2019-05-15 17:20:51 -0400

    Initial upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//this is where the JS goes
+$("#IMG-crownShyness").on('click', function() {
+	console.log("IMG-crownShyness clicked")
+	window.location.href = "https://en.wikipedia.org/wiki/Crown_shyness";
+});
